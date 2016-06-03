2d3ebd228b19fe3b1de514ab097315d898ebd457
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2016-06-03 20:10:47 +0200

    DateTime with Background

---
+++ Threading/src/org/campu02/threading/console/BackgroundWorker.java
@@ -0,0 +1,29 @@
+package org.campu02.threading.console;
+
+import java.util.Date;
+
+public class BackgroundWorker implements Runnable {
+
+	private boolean shouldRun = true;
+	
+	public void stopWorker()
+	{
+		this.shouldRun = false;
+	}
+	
+	@Override
+	public void run() {
+		while (shouldRun)
+		{
+			Date d = new Date();
+			System.out.println(d.toString());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
